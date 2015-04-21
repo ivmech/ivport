eecd547b1ea544934e93af148444e0720348b53e
Ivmech Mechatronics <bilgi@ivmech.com>
2015-04-21 15:44:18 +0300

    License GPLv3 headers were added.

--- ivport_capture_A.py
+++ ivport_capture_A.py
@@ -1,3 +1,31 @@
+#!/usr/bin/python
+#
+# This file is part of Ivport.
+# Copyright (C) 2015 Ivmech Mechatronics Ltd. <bilgi@ivmech.com>
+#
+# Ivport is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ivport is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#title           :ivport_capture_A.py
+#description     :capture ivport camera multiplexer
+#author          :Caner Durmusoglu
+#date            :20150425
+#version         :0.1
+#usage           :python ivport_capture_A.py
+#notes           :A indicates that Ivport jumper setting
+#python_version  :2.7
+#==============================================================================
+
 import RPi.GPIO as gp
 import os
 
